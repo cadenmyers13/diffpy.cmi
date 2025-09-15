8b5812ee0ee9314ea578a6097f224136f449bc7c
Caden Myers <cjm2304@columbia.edu>
2025-09-15 13:48:25 -0400

    reconfigure to run in pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+import importlib.util
 import json
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import pytest
+
+__examples_dir__ = Path(__file__).parent.parent / "docs" / "examples"
 
 
 @pytest.fixture
@@ -17,3 +21,24 @@ def user_filesystem(tmp_path):
         json.dump(home_config_data, f)
 
     yield tmp_path
+
+
+def load_module_from_path(path: Path):
+    """Load a module given an absolute Path."""
+    spec = importlib.util.spec_from_file_location(path.stem, path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+@pytest.fixture
+def suppress_plots(monkeypatch):
+    """Fixture to suppress plt.show during tests."""
+    monkeypatch.setattr(plt, "show", lambda *a, **k: None)
+
+
+def run_cmi_script(script_path: Path, suppress_plots):
+    """General runner for example scripts with a main()."""
+    module = load_module_from_path(script_path)
+    assert hasattr(module, "main"), f"{script_path} has no main() function"
+    module.main()
---
+++ tests/test_ch05.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+from conftest import __examples_dir__, run_cmi_script
+
+
+@pytest.mark.parametrize(
+    "relative_path",
+    [
+        f"{__examples_dir__}/ch05Fit2Phase/solutions/diffpy-cmi/fit2P.py",
+    ],
+)
+def test_ch05_examples(suppress_plots, relative_path):
+    script_path = Path(__file__).parent.parent / relative_path
+    run_cmi_script(script_path, suppress_plots)
