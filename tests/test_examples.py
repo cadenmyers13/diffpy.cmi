4c20e510ff15852d932926ef77e2cb0e85ced4da
Caden Myers <cjm2304@columbia.edu>
2025-09-25 11:27:05 -0400

    modify test_examples to run differently based off push or merge

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,13 +1,16 @@
+import os
 import runpy
 
 
 def test_all_examples(tmp_examples):
-    """Run all example scripts to ensure they execute without error."""
+    """Run all or selected example scripts depending on FAST_EXAMPLES
+    var in CI."""
     scripts = list(tmp_examples.rglob("**/solutions/diffpy-cmi/*.py"))
-    # sort list so that fitBulkNi.py runs first
+    # sort so fitBulkNi.py runs first
     scripts.sort(key=lambda s: 0 if s.name == "fitBulkNi.py" else 1)
+    # FAST_EXAMPLES=1 -> only run fitBulkNi.py
+    if os.getenv("FAST_EXAMPLES") == "1":
+        scripts = [s for s in scripts if s.name == "fitBulkNi.py"]
     for script in scripts:
-        script_relative_path = script.relative_to(tmp_examples).as_posix()
-        print("hello", script_relative_path)
-        print(f"Testing {script_relative_path}")
+        print(f"Testing {script}")
         runpy.run_path(str(script), run_name="__main__")
